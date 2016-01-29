e93bd0ba2f053fa404958cb758c59c9163b8b77d
ahmedfekry <ahmedfikry78@gmail.com>
2016-01-30 00:48:02 +0200

    1_fekry adding registered user class

---
+++ models/admin.php
@@ -0,0 +1,22 @@
+<?php 
+	
+	/**
+	* 
+	*/
+	class Admin extends User
+	{
+		private mobile_number;
+		function __construct($id,$name,$user_name,$email,$password,$facebook_key,$followers,$following)
+		{
+			this->id = $id;
+			this->name = $name;
+			this->email = $email;
+			this->password = $password;
+			this->facebook_key = $facebook_key;
+			this->followers = $followers;
+			this->following = $following;
+		}
+	}
+
+
+ ?>
