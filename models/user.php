9ae1cef7b46d26be3e28d38d24650d726ad8abe8
ahmedfekry <ahmedfikry78@gmail.com>
2016-01-29 23:58:17 +0200

    1_fekry adding user clas

---
+++ models/user.php
@@ -0,0 +1,19 @@
+<?php 
+	/**
+	* 
+	*/
+	class User 
+	{
+		protected $id;
+		protected $name;
+		protected $userName;
+		protected $email;
+		protected $password;
+		protected $facbookKey;
+		function __construct($id,$name,$userName,$email,$password,$facbookKey)
+		{	
+
+		}
+	}
+
+ ?>
